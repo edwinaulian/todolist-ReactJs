3895a6c2ce7cae799191bb7ec88bf0ba4a280943
edwinauliano <edwinauliano@gmail.com>
2022-08-12 11:38:48 +0700

    add all fileds build

--- dist/assets/modalAlertAction.10d7047d.js
+++
@@ -1,1 +0,0 @@
-import{S as t}from"./index.bfbe4fdd.js";const s=o=>({type:t,payload:o});export{s};
---
+++ dist/assets/modalAlertAction.2f63429f.js
@@ -0,0 +1,1 @@
+import{S as t}from"./index.0c6ae108.js";const s=o=>({type:t,payload:o});export{s};
